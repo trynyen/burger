d19de1426234a286eb44c5b853374222091ecac0
trynyen <tryn.yen@gmail.com>
2019-07-10 22:53:20 -0500

    added orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,49 @@
 var connection = require("../config/connection.js");
 
-// selectAll();
-//insertOne();
-//updateOne();
+var orm = {
+selectAll: function(tableInput, callbackfn){
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString,function(err,result){
+        if (err) throw err;
+        callbackfn(result);
+    })
+},
+
+insertOne: function(table,cols,vals,callbackfn){
+    var queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += vals.length;
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err,result){
+        if (err) throw err;
+        callbackfn(result);
+    })
+},
+
+updateOne: function(table, objColVals, condition, callbackfn) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      callbackfn(result);
+    });
+  }
+
+}
 
 module.exports = orm;
